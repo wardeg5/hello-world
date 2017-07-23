4e8dddd0722ac75387f3dd303f559c2fa9a71b17
Warren Degenhardt <wardeg5@gmail.com>
2017-07-22 19:01:27 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -3,5 +3,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
