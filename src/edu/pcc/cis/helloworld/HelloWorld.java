c0bffd4ca15b5113b40b8d6320f4306ee794b2db
Warren Degenhardt <wardeg5@gmail.com>
2017-07-23 12:09:35 -0700

    added print statement

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,6 +4,9 @@ import java.util.Date;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println(new Date());
+        System.out.println("Today is " + new Date());
+        int x = 3;
+        int y = 5;
+        System.out.println(x + " and " + y + " is " + (x + y));
     }
 }
